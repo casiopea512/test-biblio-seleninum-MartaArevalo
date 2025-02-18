2faa04d0349df0bcc74bb818087d95d6a608f31f
Marta Arevalo <martaarevaloprieto@gmail.com>
2025-02-18 17:57:16 +0100

    [TEST 2b - DONE]

---
+++ 02b-login-ko.js
@@ -0,0 +1,42 @@
+const { BaseTest } = require("./BaseTest.js")
+const { By, until } = require("selenium-webdriver");
+const assert = require('assert');
+require('dotenv').config();
+
+class MyTest extends BaseTest {
+    async test() {
+        var site = process.env.urlLogin;
+        await this.driver.get(site + "/admin/login");
+
+        // Buscar los campos de usuario y contraseña
+        let inputUserName = await this.driver.wait(until.elementLocated(By.id("id_username")), 10000);
+        let inputUserPassword = await this.driver.wait(until.elementLocated(By.id("id_password")), 10000);
+
+        // Ingresar credenciales incorrectas
+        inputUserName.sendKeys('usuariIncorrecte');
+        inputUserPassword.sendKeys('contrasenyaIncorrecta');
+
+        // Hacer clic en el botón de login
+        let submitButton = await this.driver.wait(until.elementLocated(By.css("input[value='Iniciar sessió']")), 10000);
+        submitButton.click();
+
+        try {
+            // Esperar mensaje de error de login
+            let errorMessage = await this.driver.wait(
+                until.elementLocated(By.css(".errornote")), 5000
+            );
+            let errorText = await errorMessage.getText();
+
+            console.log("TEST OK - Mensaje del incorrecto detectado: " + errorText);
+        } catch (error) {
+            assert.fail("TEST FALLIDO - No se encontró mensaje de error de login");
+        }
+    }
+}
+
+// Ejecutar el test
+(async function test_example() {
+    const test = new MyTest();
+    await test.run();
+    console.log("END");
+})();
